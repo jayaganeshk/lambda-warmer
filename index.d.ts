d05df3366c2bd680d31a93e3b947d95a551ae496
Jaya Ganesh <jaya.ganesh@wipro.com>
2022-12-24 17:38:02 +0530

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 type WarmerConfig = {
-  flag?: string,
-  concurrency?: string,
-  test?: string,
-  log?: boolean,
-  correlationId?: string,
-  delay?: number,
-};
+  flag?: string
+  concurrency?: string
+  test?: string
+  log?: boolean
+  correlationId?: string
+  delay?: number
+}
 
 /**
  * Returns a Promise that resolves to true if the current invocation is a warming
@@ -13,13 +13,18 @@ type WarmerConfig = {
  * wait until the delay specified by `config.delay` has passed.
  *
  * @param event the event passed to the lambda
+ * @param lambda context of Lambda service
  * @param [config] the config options to change lambda warmer's default behavior.  All of
  * the settings are optional
  *
  * @returns a Promise that resolves to true if this is a warming invocation
  */
-declare function warmer(event: any, config?: WarmerConfig): Promise<boolean>;
+declare function warmer(
+  event: any,
+  lambda: any,
+  config?: WarmerConfig
+): Promise<boolean>
 
-warmer.WarmerConfig = WarmerConfig;
+warmer.WarmerConfig = WarmerConfig
 
-export = warmer;
+export = warmer
